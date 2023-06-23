20930ac543ac5c0b8208c743b8297007c5f746af
DFernando30 <dfbolanos16@misena.edu.co>
2023-06-22 23:02:52 -0500

    Jueves 22 a las 11:00pm

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -1,19 +1,37 @@
-// <!-- ======= Aqui trabajaremos los cambios de los 3 perfiles con ( Switch ) ======= -->
+// Aqui trabajaremos los cambios de los 3 perfiles con ( prompt IF / ELSE ).
 
 
-// <!-- ======= Inicializar las variables que se van a usar  ======= -->
-
-
-
-// 
-
-
+// <!-- ======= Ingreso de número con prompt ======= -->
+let user, pass;
 let inicio;
 let tutores ;
 let rutasGuardadas ;
 let miMenor ;
 let imgfondo ;
 let registrate ;
+// let welcome;
+
+function comenzar (user, inicio, tutores, rutasGuardadas, miMenor, imgfondo, registrate){ 
+user = document.getElementById('inicioDocumento').value;
+pass = document.getElementById('inicioContraseña').value;
+
+console.log(user, pass);    
+
+
+// if (user < 1 || ingreso >3){
+//     alert("Tu número no cumple el rango de 1 a 3");
+// }
+
+// if (ingreso !== parseInt){
+//     alert("Necesito un número entero, no letras");
+// }
+
+// <!-- ======= Ingreso 1 - TUTOR ======= -->
+
+
+// -------------------------------------------------------------------------------------------------------- //
+
+// <!-- ======= Dar la condición a cada variable  ======= -->
 
 inicio = document.getElementById('inicio');
 tutores = document.getElementById('tutores');
@@ -21,99 +39,132 @@ rutasGuardadas = document.getElementById('rutasGuardadas');
 miMenor = document.getElementById('miMenor');
 imgfondo = document.getElementById('hero');
 registrate = document.getElementById('registrate');
+welcome = document.getElementById('welcome');
 
-let user, pass;
 
-function comenzar(user){ 
+if(user == '2556678'){
 
-user = document.getElementById('inicioDocumento').value;
-pass = document.getElementById('inicioContraseña').value;
-user.toString();
-console.log(user);
-console.log(pass);
+       // alert("Bienvenido Tutor");
 
-switch(user){
-    case "2556678":
+       window.open("Home.html", "_self");
 
-        // alert("Bienvenido Tutor");
-        console.log("Bienvenido Tutor")
-        document.querySelector('.container h1').textContent = "WELCOME TUTOR";
-        inicio.textContent   =    'Inicio';
-        tutores.textContent =    'Tutores';
-        rutasGuardadas.textContent  =    'Rutas guardadas';
-        miMenor.textContent    =    'Menor a cargo';
-    
-        registrate.remove('registrate');
-
-    
-        break;
-        
-    case "2":
-        // alert("Bienvenido Adolecente");
-        console.log("Bienvenido Menor")
-        document.querySelector('.container h1').textContent = "WELCOME Adolecente";
-        inicio.textContent   =    'MENOR';
-        tutores.textContent =    'MENOR';
-        rutasGuardadas.textContent  =    'MENOR';
-        miMenor.textContent    =    'MENOR';
-    
-          // <!-- ======= Cambio de imagen de fondo ======= -->
-    
-       imgfondo.style.backgroundImage = "url('assets/img/menor2.jpg')";
-
-    // <!-- ======= Quiutar botón de registro ======= -->
-
+       console.log("Bienvenido Tutor")
+       document.querySelector('.container h1').textContent = "WELCOME TUTOR";
+       inicio.textContent   =    'Inicio';
+       tutores.textContent =    'Tutores';
+       rutasGuardadas.textContent  =    'Rutas guardadas';
+       miMenor.textContent    =    'Menor a cargo';
+   
        registrate.remove('registrate');
-        
-    
-    //    //CAMBIAR COLOR
-    
-    //     const heading = document.querySelector('h1');
-    //     heading.style.backgroundColor = 'red';
-    
-    //     //CAMBIAR IMG
-    
-    //     const imagen = document.querySelector('.card img');
-    //     imagen.src = 'img/logo.jpg';
-    
-        break;
-        
-    case "3":
+   
 
-        alert("Bienvenido a ConfiApp");
-        document.querySelector('.container h1').textContent = "WELCOME A CONFIAPP";
-        tutores.textContent = 'Nosotros';
-        tutores.href = 'nosotros.html';
-    
-        rutasGuardadas.textContent = 'Tutores';
-        rutasGuardadas.href = 'tutores.html';
-    
-        miMenor.textContent = 'Foros';
-        miMenor.href = 'foros.html';
-    
-       // <!-- ======= Cambio de imagen de fondo ======= -->
-    
-       imgfondo.style.backgroundImage = "url('assets/img/flia1.jpg')";
-    
-    
-       // <!-- ======= Cambio contenido de Registra al menor por ======= -->
-    
-       registrate.textContent = 'REGISTRATE';
+       console.log(user, pass);    
 
+}
+
+// <!-- ======= Ingreso 2 - MENOR ======= -->
+
+
+if(user == '2'){
     
-        //CAMBIAR COLOR
-        // const heading = document.querySelector('h1');
-        // heading.style.backgroundColor = 'orange';
-    
-       //CAMBIAR IMG
-       const imagen = document.querySelector('.card img');
-       imagen.src = 'img/lupa.jpg';
+    // alert("Bienvenido Adolecente");
+
+    window.open("Home.html", "_self");
+
+    console.log("Bienvenido Menor");
+    document.querySelector('.container h1').textContent = "WELCOME Adolecente";
+
+
+     // Cambiar el menú para cada página
+
+    inicio.textContent   =    'Ruta activa';
+    tutores.textContent =    'Mensajes';
+    rutasGuardadas.textContent  =    'Rutas';
+    miMenor.textContent    =    'Mi ruta activa';
+
+    // Cambiar el redirigimiento de las páginas
+
+    inicio.href   =    '';
+    tutores.href = 'https://messages.google.com/intl/es-419/';
+    rutasGuardadas.href = 'https://www.waze.com/es-419/live-map/';
+    miMenor.href = 'https://eldiariony.com/2014/10/30/ensena-a-los-pequenos-a-cuidarse-en-la-calle/';
+
+
+    // Colocar una simulación de ruta activa, reemplazando imagen de presentación
+
+    //  welcome.innerHtml(<iframe src="https://www.google.com/maps/dir/Campanario+Centro+Comercial,+Campanario,+Carrera+9,+Comuna+1,+Popay%C3%A1n,+Cauca/SENA,+Pan-American+Highway,+Popay%C3%A1n,+Cauca/@2.4707615,-76.5989052,14z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x8e30039d7270d777:0xa749ba51fed22c24!2m2!1d-76.5937826!2d2.459386!1m5!1m1!1s0x8e300410eb607c65:0x614545787e90bea6!2m2!1d-76.56227!2d2.4827633?entry=ttu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>);
 
 
 
-        break;
 
-    default:
-        break;
+
+      // <!-- ======= Cambio de imagen de fondo ======= -->
+   imgfondo.style.backgroundImage = "url('assets/img/menor2.jpg')";
+
+
+// <!-- ======= Quiutar botón de registro ======= -->
+
+   registrate.remove('registrate');
+    
+
+//    //CAMBIAR COLOR
+
+//     const heading = document.querySelector('h1');
+//     heading.style.backgroundColor = 'red';
+
+//     //CAMBIAR IMG
+
+//     const imagen = document.querySelector('.card img');
+//     imagen.src = 'img/logo.jpg';
+
+console.log(user, pass);    
+
+
+}
+
+// <!-- ======= Ingreso 3 - CONFIAPP ======= -->
+
+
+if(user == '3'){
+
+
+    window.open("Home.html", "_self");
+    alert("Bienvenido a ConfiApp");
+    document.querySelector('.container h1').textContent = "WELCOME A CONFIAPP";
+
+
+     // Cambiar el menú para cada página
+
+    tutores.textContent = 'Nosotros';
+    rutasGuardadas.textContent = 'Tutores';
+    miMenor.textContent = 'Foros';
+
+
+     // Cambiar el redirigimiento de las páginas
+
+    tutores.href = '';
+    rutasGuardadas.href = '';
+    miMenor.href = '';
+
+   // <!-- ======= Cambio de imagen de fondo ======= -->
+
+   imgfondo.style.backgroundImage = "url('assets/img/flia1.jpg')";
+
+
+   // <!-- ======= Cambio contenido de Registra al menor por ======= -->
+
+   registrate.textContent = 'REGISTRATE';
+
+
+    //CAMBIAR COLOR
+    // const heading = document.querySelector('h1');
+    // heading.style.backgroundColor = 'orange';
+
+   //CAMBIAR IMG
+//    const imagen = document.querySelector('.card img');
+//    imagen.src = 'img/lupa.jpg';
+
+console.log(user, pass);    
+
 }
 }
